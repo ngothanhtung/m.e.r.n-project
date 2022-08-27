48460a34a8a433a3465efd3ad38d0377f3d04f77
Ngô Thanh Tùng <tungnt@softech.vn>
2022-08-27 18:32:11 +0700

    Update show

--- nextjs/pages/api/products/index.js
+++ nextjs/pages/api/products/index.js
@@ -2,5 +2,9 @@ import { NextApiResponse, NextApiRequest } from 'next';
 import products from '../../../data/products';
 
 export default function handler(req, res) {
+  if (req.method === 'POST') {
+    return res.status(200).json(products);
+  }
+
   return res.status(200).json(products);
 }
